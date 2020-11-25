eff0ab09b56dace44737db6f64aec77edc56318a
MetroidFanMan <emb1270@gmail.com>
2020-11-25 00:17:05 -0600

    Cleaned up code in app.js

--- app.js
+++ app.js
@@ -1,63 +1,61 @@
-const express = require("express");
-const request = require("request");
-const bodyParser = require("body-parser");
-const https = require("https");
-const mailChimp = require(__dirname + "/mailchimpapi.js");
-const app = express();
+const express = require('express')
+// const request = require('request')
+const bodyParser = require('body-parser')
+const https = require('https')
+const mailChimp = require(__dirname.join('/mailchimpapi.js'))
+const app = express()
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/signup.html");
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname.join('/signup.html'))
+})
 
-app.post("/", function(req, res) {
-  const firstName = req.body.fName;
-  const lastName = req.body.lName;
-  const email = req.body.email;
+app.post('/', (req, res) => {
+  const firstName = req.body.fName
+  const lastName = req.body.lName
+  const email = req.body.email
 
   const data = {
     members: [{
       email_address: email,
-      status: "subscribed",
+      status: 'subscribed',
       merge_fields: {
         FNAME: firstName,
         LNAME: lastName
       }
     }]
-  };
-
-//Declaring variables to make parameters cleaner
-  const jsonData = JSON.stringify(data);
-  const url = "https://us10.api.mailchimp.com/3.0/lists/" + mailChimp.listId;
-  const options = {
-    method: "POST",
-    auth: "ethan1:" + mailChimp.authId
   }
 
-  const request = https.request(url, options, function(response) {
+  // Declaring variables to make parameters cleaner
+  const jsonData = JSON.stringify(data)
+  const url = 'https://us10.api.mailchimp.com/3.0/lists/' + mailChimp.listId
+  const options = {
+    method: 'POST',
+    auth: 'ethan1:' + mailChimp.authId
+  }
 
+  const request = https.request(url, options, (response) => {
     if (response.statusCode === 200) {
-      res.sendFile(__dirname + "/success.html");
+      res.sendFile(__dirname.join('/success.html'))
     } else {
-      res.sendFile(__dirname + "/failure.html");
+      res.sendFile(__dirname.join('/failure.html'))
     }
 
-    response.on("data", function(data) {
-      console.log(JSON.parse(data));
-    });
-  });
+    response.on('data', (data) => {
+      console.log(JSON.parse(data))
+    })
+  })
 
-  request.write(jsonData);
-  request.end();
+  request.write(jsonData)
+  request.end()
+})
 
-});
+app.post('/failure', (req, res) => {
+  res.redirect('/')
+})
 
-app.post("/failure", function(req, res) {
-  res.redirect("/");
-});
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server is running on port 3000");
-});
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port 3000')
+})
